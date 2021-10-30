690e340bb7cd1f88873629a0997e9fff477d5259
Syhr <syahirzakwanyusof@gmail.com>
2021-10-31 00:59:46 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,17 +97,25 @@ tabs.forEach(tab => {
 })
 
 /*==================== SWIPER CONFIG ====================*/
-let swiper = new Swiper(".portfolio__container", {
-  cssMode: true,
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
   loop: true,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
   },
 });
 
